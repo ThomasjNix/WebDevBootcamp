5310f4dcbfb6a6cd303a69fb5a45f5579a130a03
Thomas Nix <tnix2@uncc.edu>
2017-07-22 12:12:53 -0400

    Added better way to completed DOMExercise03.

--- session14 - Advanced DOM Manipulation/DOMExercise03/DOMExercise03.js
+++ session14 - Advanced DOM Manipulation/DOMExercise03/DOMExercise03.js
@@ -1,17 +1,26 @@
-// On a button click, change the background from white to purple or from purple to white
+// // On a button click, change the background from white to purple or from purple to white
 
-// Set variables for the button and body
-var button = document.querySelector('#colorChangeBtn');
-var body = document.querySelector('body');
+// // Set variables for the button and body
+ var button = document.querySelector('#colorChangeBtn');
+ var body = document.querySelector('body');
 
-// Set the background color to be white for the click event so I don't have to use rgb()
-body.style.background = "white"; 
+// // Set the background color to be white for the click event so I don't have to use rgb()
+// body.style.background = "white"; 
 
-// Add a click event with conditional anonymous function to change the body color
+// // Add a click event with conditional anonymous function to change the body color
+// button.addEventListener('click', function(){
+// 	if (body.style.background === "white"){
+// 		body.style.background = "purple";
+// 	}else{
+// 		body.style.background = "white";
+// 	}
+// });
+
+
+/*A better way of doing things */
+
+//Create a CSS class that sets background color to purple
+
 button.addEventListener('click', function(){
-	if (body.style.background === "white"){
-		body.style.background = "purple";
-	}else{
-		body.style.background = "white";
-	}
+	body.classList.toggle('togglePurple');
 });
